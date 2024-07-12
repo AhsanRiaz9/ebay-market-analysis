47dcc2c2aa784e395ebad1dec5caa688fe91c0c3
AhsanRiaz9 <ahsan.m.riaz87004@gmail.com>
2024-07-12 10:56:28 +0500

    feat: setup gitignore, requirments.txt and .env file

---
+++ readme.md
@@ -0,0 +1,41 @@
+## Ebay Analytics Portal
+    
+## Pre-Requisite:
+- python3
+- pip
+- mysql
+
+## Create Virtual Environment
+ ### For windows:
+    python -m venv venv
+
+ ### For Linux:
+    python3 -m venv venv
+
+## Activate Virtual Environment
+ ### For windows:
+    venv/Scripts/activate
+
+ ### For Linux:
+    source venv/bin/activate
+
+
+## Create .env file
+- replace .env.example with .env
+- Place your credentials in .env
+
+## Install Required packages
+    pip install - requirements.txt
+
+## Run Database migration
+    python3 manage.py makemigrations
+    python3 manage.py migrate
+
+## Run Django Project:
+    python3 manage.py runserver
+
+Now, your django server will run at port 8000
+http://127.0.0.1:8000/
+
+If you face issue related to mysqlclient installation package use this documentation
+https://pypi.org/project/mysqlclient/
---
+++ settings/utilis/helpers.py
@@ -0,0 +1,54 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class SeleniumWebDriver:
+    def __init__(self, headless=True):
+        self.driver = self._configure_webdriver(headless)
+        
+    def _configure_webdriver(self, headless):
+        options = Options()
+        self._configure_chrome_options(options, headless)
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--start-maximized")
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    @staticmethod
+    def _configure_chrome_options(options, headless):
+        if headless:
+            options.add_argument('--headless=new')
+        options.add_argument('--no-sandbox')
+
+    def close(self):
+        try:
+            self.driver.close()
+        except:
+            pass
+
+def create_encoded_url(url, params):
+    conditions = [f'{key}={params[key]}' for key in params.keys()]
+    query_string = '&'.join(conditions)
+    if not '?' in url:
+        encoded_url = f'{url}?{query_string}'   
+    else:
+        encoded_url = f'{url}{query_string}'
+    encoded_url = encoded_url.replace(' ', '%2520').replace(',', '%7C')
+    return encoded_url
+
+def format_date(date_str):
+    date_str = date_str.replace('SOLD  ', '')
+    date_info = date_str.split(' ')
+    month_dict = {'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04', 'may': '05', 'jun': '06', 'jul': '07', 'aug': '08', 'sep': '09', 'oct': '10', 'nov': '11', 'dec': '12'}
+    day = date_info[0]
+    if len(day) == 1:
+        day = f'0{day}'
+    month = date_info[1].lower()
+    year = date_info[2]
+    formated_date = f'{year}-{month_dict[month]}-{day}'
+    return formated_date
+
