02155f174c9e3ca5f948a9184cdc30c36f9e12ea
AhsanRiaz9 <ahsan.m.riaz87004@gmail.com>
2024-07-31 11:47:02 +0500

    fix: updated slack message for active mobile data

--- ebay_marketing_analysis_backend/scraping_scheduler/tasks.py
+++ ebay_marketing_analysis_backend/scraping_scheduler/tasks.py
@@ -3,7 +3,7 @@ from product_configuration.models import Category
 from scraping_scheduler.models import ScrapingProcess, MobileScrapingProcess
 from django.utils import timezone
 from datetime import timedelta
-from ebay_products.models import MobilePhone
+from ebay_products.models import MobilePhone, ActiveMobilePhone
 import os
 import requests
 import datetime
@@ -13,11 +13,11 @@ from settings.utilis.slack_bot import generate_mobile_phone_scraping_report
 def mobile_phone_scraping_scheduler(category_id, new_process=False, first_process=False):
     print('hitting mobile phone scheduler')
     category = Category.objects.filter(ebay_category_id=category_id).first()
-    today = str(datetime.datetime.now())[:10]
-    last_20_minutes = timezone.now() - timedelta(minutes=20)
-    total_mobile_phones = MobilePhone.objects.all()
-    today_scraped_phones = total_mobile_phones.filter(created_at__gte=today)
-    last_20_minutes_scraped = total_mobile_phones.filter(created_at__gte=last_20_minutes)
+    mobile_data = {
+        'sold_items': mobile_analytics(True),
+        'buy_it_now_items': mobile_analytics(False),
+    }    
+    last_20_minutes_scraped = mobile_data['sold_items']['last_20_minutes_scraped'] + mobile_data['buy_it_now_items']['last_20_minutes_scraped']
     if first_process:
         request_scraping(category_id)
     else:
@@ -44,8 +44,18 @@ def mobile_phone_scraping_scheduler(category_id, new_process=False, first_proces
             ebay_condition = ''
             mobile_model = ''
             listing_type = ''
-        generate_mobile_phone_scraping_report(category_id, ebay_condition, mobile_model, total_mobile_phones.count(), today_scraped_phones.count(), last_20_minutes_scraped.count(), listing_type, scraping_status)
+        generate_mobile_phone_scraping_report(category_id, ebay_condition, mobile_model, mobile_data, listing_type, scraping_status)
 
+def mobile_analytics(is_sold_listing):
+    today = str(datetime.datetime.now())[:10]
+    last_20_minutes = timezone.now() - timedelta(minutes=20)
+    if is_sold_listing == True:
+        total_mobile_phones = MobilePhone.objects.all()
+    else:
+        total_mobile_phones = ActiveMobilePhone.objects.all()
+    today_scraped_phones = total_mobile_phones.filter(created_at__gte=today)
+    last_20_minutes_scraped = total_mobile_phones.filter(created_at__gte=last_20_minutes)
+    return {'total_scraped': total_mobile_phones.count(), 'today_scraped': today_scraped_phones.count(), 'last_20_minutes_scraped': last_20_minutes_scraped.count()}
 
 def request_scraping(category_id):
     try:
--- ebay_marketing_analysis_backend/settings/utilis/slack_bot.py
+++ ebay_marketing_analysis_backend/settings/utilis/slack_bot.py
@@ -22,7 +22,7 @@ def send_message(channel_id, message_text):
             print(f"Got an error: {e.response['error']}")
 
 
-def generate_mobile_phone_scraping_report(ebay_category_id, current_condition, current_mobile_phone, total_scraped, today_scraped, last_20_minutes_scraped, listing_type, scraping_status):
+def generate_mobile_phone_scraping_report(ebay_category_id, current_condition, current_mobile_phone, mobile_data, listing_type, scraping_status):
     current_timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     report_message = f"""
     📱 *Mobile Phone Scraping Report* 📱
@@ -31,9 +31,16 @@ def generate_mobile_phone_scraping_report(ebay_category_id, current_condition, c
 
     📂 *eBay Category ID:* {ebay_category_id}
 
-    *Total Mobile Phones Scraped:* {total_scraped}
-    *Total Scraped Today:* {today_scraped}
-    *Scraped in Last 20 Minutes:* {last_20_minutes_scraped}
+    * *Sold Items Report:* *
+    *Total Mobile Phones Scraped:* {mobile_data['sold_items']['total_scraped']}
+    *Total Scraped Today:* {mobile_data['sold_items']['today_scraped']}
+    *Scraped in Last 20 Minutes:* {mobile_data['sold_items']['last_20_minutes_scraped']}
+    
+    * *But It Now Items Report:* *
+    *Total Mobile Phones Scraped:* {mobile_data['buy_it_now_items']['total_scraped']}
+    *Total Scraped Today:* {mobile_data['buy_it_now_items']['today_scraped']}
+    *Scraped in Last 20 Minutes:* {mobile_data['buy_it_now_items']['last_20_minutes_scraped']}
+    
     *Currently Scraping:* {current_mobile_phone}
     *Condition:* {current_condition}
     *Listing Type:* {listing_type}
