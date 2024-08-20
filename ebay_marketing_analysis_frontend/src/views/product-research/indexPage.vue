556856efedb00c2fd84f337de2e505d164d67ead
alishigri1-admin <ali.shigri@oztechwork.com>
2024-08-20 15:08:45 +0500

    feat: some ui issue has been fixed moreover some feature has been disabled for user role, only shown to super admin

--- ebay_marketing_analysis_frontend/src/views/product-research/indexPage.vue
+++ ebay_marketing_analysis_frontend/src/views/product-research/indexPage.vue
@@ -13,47 +13,47 @@
         <div class="section">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">marketplace</h6>
+            <h6 class="fw-semibold">Marketplace</h6>
             <exclamation-circle-icon />
           </div>
-          <v-select v-model="data.name" clearable class="selectinput" label="select marketplace" :items="['ebay.com.au']" @update:modelValue="callproductsApi()" variant="underlined"></v-select>
+          <v-select v-model="data.name" clearable class="selectinput" label="Select marketplace" :items="['ebay.com.au']" @update:modelValue="callproductsApi()" variant="underlined"></v-select>
         </div>
       </div>
       <div class="col-lg-4 col-md-6 col-sm-12 col-xl-3">
         <div class="section">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">shipping location</h6>
+            <h6 class="fw-semibold">Shipping location</h6>
             <exclamation-circle-icon />
           </div>
-          <v-select clearable class="selectinput" v-model="data.shippingLocation" label="select shipping location" :items="['Australia']" variant="underlined" @update:modelValue="callproductsApi()"></v-select>
+          <v-select clearable class="selectinput" v-model="data.shippingLocation" label="Select shipping location" :items="['Australia']" variant="underlined" @update:modelValue="callproductsApi()"></v-select>
         </div>
       </div>
       <div class="col-lg-4 col-md-6 col-sm-12 col-xl-3">
         <div class="section">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">data category</h6>
+            <h6 class="fw-semibold">Data category</h6>
             <exclamation-circle-icon />
           </div>
-          <v-select class="selectinput" v-model="data.dataCategory" label="select data category" :items="['Active', 'Sold']" variant="underlined" @update:modelValue="callproductsApi()"></v-select>
+          <v-select class="selectinput" v-model="data.dataCategory" label="Select data category" :items="['Active', 'Sold']" variant="underlined" @update:modelValue="callproductsApi()"></v-select>
         </div>
       </div>
       <div class="col-lg-4 col-md-6 col-sm-12 col-xl-3">
         <div class="section">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">price</h6>
+            <h6 class="fw-semibold">Price</h6>
             <exclamation-circle-icon />
           </div>
           <div class="mt-auto gap-2 d-flex justify-content-around align-items-center px-4 pb-3">
             <div class="w-50">
               <input @change="checkPrice" v-model="data.minPrice" type="number" class="pricerangeinput" />
-              <b class="text-center mx-auto d-block">min</b>
+              <b class="text-center mx-auto d-block">Min</b>
             </div>
             <div class="w-50">
               <input @change="checkPrice" v-model="data.maxPrice" type="number" class="pricerangeinput" />
-              <b class="text-center mx-auto d-block">max</b>
+              <b class="text-center mx-auto d-block">Max</b>
             </div>
           </div>
         </div>
@@ -62,7 +62,7 @@
         <div class="section">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">sale date range</h6>
+            <h6 class="fw-semibold">Sale date range</h6>
             <exclamation-circle-icon />
           </div>
 
@@ -73,20 +73,20 @@
         <div class="section">
           <div input class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">exclude phrase</h6>
+            <h6 class="fw-semibold">Exclude phrase</h6>
             <exclamation-circle-icon />
           </div>
-          <input class="pt-5 mt-2 exclude-phrase" v-model="data.excludedPhrase" @input="callproductsApi()" style="border-bottom: 2px solid black" placeholder="comma separated phrase" type="text" />
+          <input class="pt-5 mt-2 exclude-phrase" v-model="data.excludedPhrase" @input="callproductsApi()" style="border-bottom: 2px solid black" placeholder="Comma separated phrase" type="text" />
         </div>
       </div>
       <div v-for="(item, index) in productfilter" :key="index" class="col-lg-4 col-md-6 col-sm-12 col-xl-3">
         <div class="section" v-if="productfilter">
           <div class="d-flex justify-content-between">
             <div></div>
-            <h6 class="fw-semibold">{{ index }}</h6>
+            <h6 class="fw-semibold">{{ index.charAt(0).toUpperCase() + index.slice(1) }}</h6>
             <exclamation-circle-icon />
           </div>
-          <v-autocomplete style="font-weight: 700;" multiple v-model="data[index]" @update:modelValue="callproductsApi()" class="selectinput" clearable :label="index" :items="item && item?.map((items) => ({ name: items?.name || items?.value, id: items?.ebay_condition_id || items?.id }))" item-title="name" item-value="id" variant="underlined"></v-autocomplete>
+          <v-autocomplete style="font-weight: 700;" multiple v-model="data[index]" @update:modelValue="callproductsApi()" class="selectinput" clearable :label="index?.charAt(0).toUpperCase() + index?.slice(1)" :items="item && item?.map((items) => ({ name: items?.name || items?.value, id: items?.ebay_condition_id || items?.id }))" item-title="name" item-value="id" variant="underlined"></v-autocomplete>
         </div>
       </div>
 
@@ -223,261 +223,6 @@
     </div>
   </div>
 </template>
-
-<!-- <script>
-  import { onMounted, ref } from 'vue'
-  import { Navigation } from 'swiper'
-  import { useStore } from 'vuex'
-  // import { Swiper, SwiperSlide } from 'swiper/vue'
-  // import AnalyticsWidget from '@/components/widgets/AnalyticsWidget.vue'
-  import AOS from 'aos'
-  export default {
-    components: {
-      // SwiperSlide,
-      // AnalyticsWidget
-    },
-    setup() {
-      const store = useStore()
-      const modules = [Navigation]
-      const grossSaleChart = ref({
-        series: [
-          {
-            name: 'total',
-            data: [94, 80, 94, 80, 94, 80, 94]
-          },
-          {
-            name: 'pipline',
-            data: [72, 60, 84, 60, 74, 60, 78]
-          }
-        ],
-        options: {
-          chart: {
-            fontFamily: '"Inter", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-            height: 245,
-            type: 'area',
-            toolbar: {
-              show: false
-            },
-            sparkline: {
-              enabled: false
-            }
-          },
-          colors: ['#3a57e8', '#079aa2'],
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'smooth',
-            width: 3
-          },
-          yaxis: {
-            show: true,
-            labels: {
-              show: true,
-              minWidth: 19,
-              maxWidth: 19,
-              style: {
-                colors: '#8A92A6'
-              },
-              offsetX: -5
-            }
-          },
-          legend: {
-            show: false
-          },
-          xaxis: {
-            labels: {
-              minHeight: 22,
-              maxHeight: 22,
-              show: true,
-              style: {
-                colors: '#8A92A6'
-              }
-            },
-            lines: {
-              show: false // or just here to disable only x axis grids
-            },
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug']
-          },
-          grid: {
-            show: false
-          },
-          fill: {
-            type: 'gradient',
-            gradient: {
-              shade: 'dark',
-              type: 'vertical',
-              shadeIntensity: 0,
-              gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
-              inverseColors: true,
-              opacityFrom: 0.4,
-              opacityTo: 0.1,
-              stops: [0, 50, 80],
-              colors: ['#3a57e8', '#4bc7d2']
-            }
-          },
-          tooltip: {
-            enabled: true
-          }
-        }
-      })
-      const earningChart = ref({
-        series: [55, 75],
-        options: {
-          chart: {
-            height: 260,
-            type: 'radialBar'
-          },
-          colors: ['#4bc7d2', '#3a57e8'],
-          plotOptions: {
-            radialBar: {
-              hollow: {
-                margin: 10,
-                size: '50%'
-              },
-              track: {
-                margin: 10,
-                strokeWidth: '50%'
-              },
-              dataLabels: {
-                show: false
-              }
-            }
-          }
-        }
-      })
-      const conversionChart = ref({
-        series: [
-          {
-            name: 'Successful deals',
-            data: [30, 50, 35, 60, 40, 60, 60, 30, 50, 35]
-          },
-          {
-            name: 'Failed deals',
-            data: [40, 50, 55, 50, 30, 80, 30, 40, 50, 55]
-          }
-        ],
-        options: {
-          chart: {
-            type: 'bar',
-            height: 230,
-            stacked: true,
-            toolbar: {
-              show: false
-            }
-          },
-          colors: ['#3a57e8', '#4bc7d2'],
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: '28%',
-              endingShape: 'rounded',
-              borderRadius: 5
-            }
-          },
-          legend: {
-            show: false
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ['transparent']
-          },
-          xaxis: {
-            categories: ['S', 'M', 'T', 'W', 'T', 'F', 'S', 'M', 'T', 'W'],
-            labels: {
-              minHeight: 20,
-              maxHeight: 20,
-              style: {
-                colors: '#8A92A6'
-              }
-            }
-          },
-          yaxis: {
-            title: {
-              text: ''
-            },
-            labels: {
-              minWidth: 19,
-              maxWidth: 19,
-              style: {
-                colors: '#8A92A6'
-              }
-            }
-          },
-          fill: {
-            opacity: 1
-          },
-          tooltip: {
-            y: {
-              formatter: function (val) {
-                return '$ ' + val + ' thousands'
-              }
-            }
-          }
-        }
-      })
-      const swiperItems = ref([
-        {
-          size: 90,
-          amount: '560K',
-          subTitle: 'Total Sales',
-          color: 'primary'
-        },
-        {
-          size: 80,
-          amount: '185K',
-          subTitle: 'Total Profit',
-          color: 'info'
-        },
-        {
-          size: 70,
-          amount: '375K',
-          subTitle: 'Total Cost',
-          color: 'primary'
-        },
-        {
-          size: 60,
-          amount: '742K',
-          subTitle: 'Revenue',
-          color: 'info'
-        },
-        {
-          size: 50,
-          amount: '150K',
-          subTitle: 'Net Income',
-          color: 'primary'
-        },
-        {
-          size: 40,
-          amount: '4600',
-          subTitle: 'Today',
-          color: 'info'
-        },
-        {
-          size: 30,
-          amount: '11.2M',
-          subTitle: 'Members',
-          color: 'primary'
-        }
-      ])
-      onMounted(() => {
-        AOS.init({
-          disable: function () {
-            var maxWidth = 996
-            return window.innerWidth < maxWidth
-          },
-          once: true,
-          duration: 800
-        })
-      })
-      return { modules,store, grossSaleChart, earningChart, conversionChart, swiperItems }
-    }
-  }
-  </script> -->
 
 <script>
 import ExclamationCircleIcon from '@/components/icons/outlined/svg-icons/ExclamationCircleIcon.vue'
